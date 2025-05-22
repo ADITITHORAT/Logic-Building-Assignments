4f4fa9ffe6f93ce8ed02d894e5f36fa9cde1ae98
ADITITHORAT <aditi.thorat2004@gmail.com>
2025-05-22 16:04:06 +0530

    Add files via upload

---
+++ Assignment15/program5.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;  //TIME COMPLEXITY O(N)
+class Pattern
+{
+  public void Display(int iRow,int iCol)
+  {
+    int i = 0,j = 0;
+   
+    for(i = 1 ;i<=iRow ;i++)
+    {
+      for(j=1;j<=iCol;j++)
+      {
+        if((i == 1) || (i == iRow) || (j == 1) || (j == iCol))
+        {
+          System.out.print(j+"\t");
+        }
+        else 
+        {
+          System.out.print("*\t");
+        }
+       
+      }
+      System.out.println("\n");
+    }
+  }
+}
+public class program5
+ {
+  public static void main(String[] args) 
+  {
+      int iValue1 = 0,iValue2 = 0;
+      Scanner sobj = new Scanner(System.in);
+      System.out.println("Enter the number of rows: ");
+      iValue1 = sobj.nextInt();
+      System.out.println("Enter the number of cols: ");
+      iValue2 = sobj.nextInt();
+      Pattern pobj = new Pattern();
+      pobj.Display(iValue1,iValue2);
+
+  }
+}
+
