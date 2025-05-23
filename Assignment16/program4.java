0171f4884d84fda9a35308ab99d25902ab72ec6c
ADITITHORAT <aditi.thorat2004@gmail.com>
2025-05-23 10:42:45 +0530

    Add files via upload

---
+++ Assignment16/program4.java
@@ -0,0 +1,46 @@
+import java.util.Scanner; // TIME COMPLEXITY O(N^2)
+class Pattern
+{
+  public void Display(int iRow,int iCol)
+  {
+    int i = 0,j = 0;
+    if(iRow != iCol)
+    {
+      System.out.println("Invalid Input");
+      return ;
+    }
+    for(i = 1 ;i<=iRow ;i++)
+    {
+      for(j=1;j<=iCol;j++)
+      {
+        if((i == j) || (i == 1) || (j == 1) || (i == iRow) ||(j == iCol))
+        {
+          System.out.print("*\t"); 
+        }
+        else
+        {
+          System.out.print("\t"); 
+        }
+              
+      }
+      System.out.println("\n");
+    }
+  }
+}
+public class program4
+ {
+  public static void main(String[] args) 
+  {
+      int iValue1 = 0,iValue2 = 0;
+      Scanner sobj = new Scanner(System.in);
+      System.out.println("Enter the number of rows: ");
+      iValue1 = sobj.nextInt();
+      System.out.println("Enter the number of cols: ");
+      iValue2 = sobj.nextInt();
+      Pattern pobj = new Pattern();
+      pobj.Display(iValue1,iValue2);
+
+  }
+}
+
+
