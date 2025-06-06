7150c376d702ff1275a0ff49ad446869ac3d8290
ADITITHORAT <aditi.thorat2004@gmail.com>
2025-06-06 10:42:00 +0530

    Add files via upload

---
+++ Assignment27/program5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void StrCatX(char *src, char *dest)
+{
+  while(*src != '\0')
+  {
+    src++;
+  }
+  
+  while (*dest != '\0')
+  {
+    *src = *dest;
+    dest++; 
+    src++;
+  }
+  *src = '\0';
+  
+}
+
+int main()
+{
+  char arr[30] = "Marvellous Multi OS";
+  char brr[30] = " Logic building";
+  StrCatX(arr,brr);
+  printf("%s",arr);
+  return 0;
+}
